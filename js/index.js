c1e5f5649206b314c1e2a22ed86e9e66081dd04a
KAUAN_DEV⌨️ <127789874+kaka-bip@users.noreply.github.com>
2026-08-01 20:25:52 -0300

    .

--- js/index.js
+++
@@ -1,8 +0,0 @@
-    let typed = new Typed(".typed-text", {
-      strings: ["Kauan!","Dev Web", "Front End"],
-      typeSpeed: 60,
-      backSpeed: 60,
-      backDelay: 1000,
-      startDelay: 300,
-      loop: true
-    });
