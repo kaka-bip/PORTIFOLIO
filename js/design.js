3063aaa7a9e0fd0a24eca02dcc692b2bdbaad984
KAUAN_DEV⌨️ <127789874+kaka-bip@users.noreply.github.com>
2026-08-03 22:36:01 -0300

    Full-stack.0.9

--- js/design.js
+++ js/design.js
@@ -28,3 +28,23 @@ new Typed("#typer", {
   loop: true,
   showCursor: false,
 });
+
+
+//scrolled
+
+const navbar = document.getElementById('menu');
+
+window.addEventListener('scroll', () => {
+      if (window.scrollY > 20) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+//loader
+
+window.addEventListener('load', () => {
+  const  loader = document.getElementById('loader');
+  loader.classList.add('hidden');
+})
